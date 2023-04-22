a8f29ed25e7659da9e08089589c0759a2a8cfa56
Trevor Keith <tk@tkeith.com>
2023-04-22 16:56:10 -0400

    bring over contract code

---
+++ contracts/src/demo.ts
@@ -0,0 +1,95 @@
+import { ConcealedCare } from './ConcealedCare.js';
+import {
+  isReady,
+  shutdown,
+  Field,
+  Mina,
+  PrivateKey,
+  AccountUpdate,
+  Bool,
+} from 'snarkyjs';
+
+await isReady;
+
+const report = {
+  patientIdHash: new Field(12345678),
+  validUntil: new Field(20230430),
+  bloodPressure: new Field(80),
+  hasConditionA: new Bool(true),
+  hasConditionB: new Bool(false),
+  hasConditionC: new Bool(true),
+};
+
+const requirements = {
+  patientIdHash: new Field(12345678),
+  verifyTime: new Field(20230422),
+  minBloodPressure: new Field(60),
+  maxBloodPressure: new Field(120),
+  allowConditionA: new Bool(true),
+  allowConditionB: new Bool(true),
+  allowConditionC: new Bool(false),
+};
+
+const useProof = false;
+const Local = Mina.LocalBlockchain({ proofsEnabled: useProof });
+Mina.setActiveInstance(Local);
+const { privateKey: deployerKey, publicKey: deployerAccount } =
+  Local.testAccounts[0];
+const { privateKey: senderKey, publicKey: senderAccount } =
+  Local.testAccounts[1];
+
+const zkAppPrivateKey = PrivateKey.random();
+const zkAppAddress = zkAppPrivateKey.toPublicKey();
+
+const zkAppInstance = new ConcealedCare(zkAppAddress);
+const deployTxn = await Mina.transaction(deployerAccount, () => {
+  AccountUpdate.fundNewAccount(deployerAccount);
+  zkAppInstance.deploy();
+});
+await deployTxn.sign([deployerKey, zkAppPrivateKey]).send();
+
+// DOCTOR PUBLISH REPORT
+
+const publishTxn = await Mina.transaction(senderAccount, () => {
+  zkAppInstance.publishReport(report);
+});
+
+await publishTxn.prove();
+const signRes = await publishTxn.sign([senderKey]).send();
+
+console.log('publishTxn.toPretty()', publishTxn.toPretty());
+console.log('signRes', signRes);
+console.log('signRes.hash()', signRes.hash());
+
+// EMPLOYEE PUBLISH PROOF
+
+const publishAccommodationProofTxn = await Mina.transaction(
+  senderAccount,
+  () => {
+    zkAppInstance.publishAccommodationProof(report, requirements);
+  }
+);
+
+await publishAccommodationProofTxn.prove();
+const publishAccommodationProofRes = await publishAccommodationProofTxn
+  .sign([senderKey])
+  .send();
+
+// EMPLOYER CHECK PROOF
+
+const verifyAccommodationProofTxn = await Mina.transaction(
+  senderAccount,
+  () => {
+    zkAppInstance.verifyAccommodationProof(requirements);
+  }
+);
+
+await verifyAccommodationProofTxn.prove();
+const verifyAccommodationProofRes = await verifyAccommodationProofTxn
+  .sign([senderKey])
+  .send();
+
+// END
+
+console.log('Shutting down');
+await shutdown();
