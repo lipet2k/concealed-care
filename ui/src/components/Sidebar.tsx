cf2e826c057a28a815d563de51622796d95bc5bc
Trevor Keith <tk@tkeith.com>
2023-04-22 18:44:20 -0400

    home page converted to next

---
+++ ui/src/components/Sidebar.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import { FiUser } from "react-icons/fi";
+import { RiHospitalFill } from "react-icons/ri";
+import { TbReportMedical } from "react-icons/tb";
+
+
+const NavigationBar = () => {
+  return (
+    <div className="sidenav flex-col flex w-48">
+      <div><img id="logo" src="/assets/cc-logo.svg"></img></div>
+      <div className="links-container ml-10">
+        <Link className="nav-item mt-5" href="/patients">
+          <button className="btn-col-main hover:bg-blue-700 text-white py-2 px-4 rounded">
+            <RiHospitalFill /> Patients
+          </button>
+        </Link>
+        <Link className="nav-item mt-5" href="/profiles">
+          <button className="bg-white text-black py-2 px-4 rounded">
+            <FiUser /> Profile
+          </button>
+        </Link>
+
+        <Link className="nav-item mt-5" href="/new_request">
+          <button className="btn-col-main hover:bg-blue-700 text-white py-2 px-4 rounded">
+            <TbReportMedical /> New Request
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+
+
+const Header = () => {
+
+  return (
+    <div className="heading-container bg-main">
+      <div className="profile-info text-lg large-font heading-font shrink-0 mx-5 mt-2 ">
+        <h3>Hospital</h3>
+        <p>MIIBOgIBAAJBAKj34GkxFhD90vcNLYL</p>
+      </div>
+      <NavigationBar />
+    </div>
+  );
+}
+
+export default Header
---
+++ ui/src/pages/new-report.page.tsx
@@ -0,0 +1,64 @@
+import Sidebar from "@/components/Sidebar";
+
+export default function NewReport() {
+  return (
+    <div className="App bg-white-50 dark:bg-zinc-900">
+      <Sidebar />
+      <div className="generate-keys">
+        <h1>New Report</h1>
+        <form className="main-form" action="" method="get">
+          <h2>Christian Adelmund</h2>
+          <p className="secondary">hdI4yZ5ew18JH4JW9jbhUFrviQzM7</p>
+
+          <div className="top-right"><button className="secondary">Import</button></div>
+
+          <div className="half-width inner-form-container">
+            <h3>Conditions</h3>
+            <input
+              className="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              id="condition-1"
+              type="text"
+              value="High Blood Pressure"
+            ></input>
+
+            <input
+              className="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              id="condition-2"
+              type="text"
+              placeholder="New Condition"
+            ></input>
+
+            <a href="#">+ Add Condition</a>
+          </div>
+
+          <div className="half-width inner-form-container second-col">
+            <h3>Accommodations</h3>
+            <input
+              className="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              id="condition-1"
+              type="text"
+              value="Reserved Parking"
+            ></input>
+
+            <input
+              className="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              id="condition-2"
+              type="text"
+              placeholder="New Accommodation"
+            ></input>
+
+            <a href="#">+ Add Accommodation</a>
+          </div>
+
+          <button
+            className="button-main right hover:bg-blue-800 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            type="button"
+          >
+            Encrypt and Publish
+          </button>
+        </form>
+      </div>
+    </div>
+
+  );
+};
